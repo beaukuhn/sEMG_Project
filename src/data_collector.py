7635aa991d87c37405311aaeaf1cf396e7d348cb
ak5793 <alexkuhn@berkeley.edu>
2020-04-21 20:29:58 -0700

    Refactored a lot of code in data_collector.py

--- src/data_collector.py
+++ src/data_collector.py
@@ -8,36 +8,83 @@ import serial
 import time
 import datetime
 import csv
-
 from utils import print_progress_bar, create_dirs, path_exists
 
+RECORDING_DURATION = 60
+WRITING_DURATION = 45
 HAND_MOTIONS = {0: 'open_palm', 1: 'closed_fist'}  # Will be added to
+BAUD_RATE = 9600
+PORT = '/dev/ttyACM0'
 
-def connect_arduino(port_str='/dev/ttyACM0', baud_rate=9600):
+arduino = serial.Serial(PORT, BAUD_RATE)
+print("Arduino connection established: {}".format(arduino.is_open))
+
+def close_connection(arduino):
     """
-    Connects to the Serial Port on the Arduino
-
-    @param port_str(str) is found by typing ls /dev/tty* in terminal
-    @param baud_rate(int) is the symbol transfer rate
-
-    @return arduino(PySerial) is the PySerial port wrapper to be read from
-    """
-    arduino = serial.Serial(port_str, baud_rate)
-    time.sleep(1)
-    print("Serial Port Open?: {}".format(arduino.is_open))
-    return arduino
-
-def terminate_process(arduino):
-    """
-    Closes serial port, effectively ending process.
+    Closes serial port connection, effectively ending process.
 
     @params:
         arduino(PySerial Obj) - Required: Reads from serial port
     """
     arduino.close()
-    print("Program terminated. Good luck with the project!\n")
+    print("Connection closed. Good luck with the project!\n")
+    return False
 
-def collect_data(data_path, hand_motion, arduino, duration=60, write_time=45):
+
+def prompt_dispatcher(function_key):
+    """
+    Higher order function that dispatches functions which initialize prompts.
+
+    @params:
+        function_key(str) - Required : Key that maps to desired function
+
+    @returns:
+        function_dispatcher[function_key](function)
+    """
+
+    def gather_parameters(key):
+        subject_query = "What is the subject number?\n"
+        motion_query = ("Which hand motion would like to collect data for?\n" +
+                        "0: open-palm, 1: closed-fist\n")
+        trial_query = "What is the trial number?\n"
+        key2query = {
+            "What is the subject number?": subject_query,
+            "What is the motion?": motion_query,
+            "What is the trial number?": trial_query,
+        }
+        try:
+            num = int(input(key2query[key]))
+            assert (num >= 0 or num in HAND_MOTIONS), "Invalid Integer Input"
+            return num
+        except ValueError:
+            print("Input must be an integer. Please try again...")
+
+    def prepare_for_collection():
+        prompt = "Prepare yourself for data collection.\nPress any key to begin"
+        input(prompt)
+
+    def is_valid_path(data_path):
+        if path_exists(data_path):
+            ans = input("{} already exists. Continue? [Y/n]\n".format(data_path))
+            if ans not in ['y', 'Y']:
+                return close_connection(arduino)
+            return True
+
+    def start_new_trial():
+        ans = input("Start trial? [y/N]\n")
+        if ans != "y":
+            return close_connection(arduino)
+        return True
+
+    function_dispatcher =  {
+        "get gather_parameters function": gather_parameters,
+        "Prepare for data collection.": prepare_for_collection,
+        "Is this a valid path?": is_valid_path,
+        "Begin a new trial?": start_new_trial,
+    }
+    return function_dispatcher[function_key]
+
+def collect_data(data_path, hand_motion):
     """
     Initializes data collection for hand_motion and writes voltage data
     to csv file located at data_path.
@@ -49,21 +96,23 @@ def collect_data(data_path, hand_motion, arduino, duration=60, write_time=45):
     @param duration(int) is the length of time in seconds for recording
     @param write_time(int) is the amount of time(seconds) to wait for data write
     """
-    # Here for priming
     print("Initializing Data Collection - Output File:{}".format(data_path))
     time.sleep(5)
-    print("Data collection started!'\nDuration - {} seconds".format(duration))
 
-    # Add names of columns
+    # Add names of columns for CSV file
     headers = ["Sensor" + str(num) for num in range(5)]
     rows = [ headers ]
 
-    # Create Datetimes that dictate data recording process
+    # Create Datetimes that dictate length of data recording
     start_time = datetime.datetime.now()
-    end_time = start_time + datetime.timedelta(seconds=duration)
+    end_time = start_time + datetime.timedelta(seconds=RECORDING_DURATION)
 
-    # Data Collection
-    while True:
+    #######################################
+    # --------- Data Recording ---------- #
+    #######################################
+    print("Data collection started!\nDuration - {} seconds".format(RECORDING_DURATION))
+    time.sleep(5)
+    while datetime.datetime.now() < end_time:
         curr_row = []
         for sensor_num in range(5):
             read_bytes = arduino.read(size=2)
@@ -72,77 +121,47 @@ def collect_data(data_path, hand_motion, arduino, duration=60, write_time=45):
             curr_row.append(val)
         print_progress_bar(datetime.datetime.now(), start_time, end_time, length=50)
         rows.append(curr_row)
-        if datetime.datetime.now() >= end_time:
-            break
 
-    # Data Writing
+    ######################################
+    #----------- Data Writing -----------#
+    ######################################
     print("Preparing file for write operation...")
     with open(data_path, 'a+', newline='') as f:
-        print(data_path)
         time.sleep(5)  # This is here to be safe
-        print("Waiting {} seconds for operation to complete...".format(write_time))
+        print("Waiting {} seconds for operation to complete...".format(WRITING_DURATION))
         writer = csv.writer(f)
         writer.writerows(rows)
-        time.sleep(write_time)
+        time.sleep(WRITING_DURATION)
+
     print("Data Collection Completed.")
 
-def begin(arduino):
+def initialize_pipeline():
     """
     Begins the data collection pipeline.
 
     @params:
         arduino (PySerial Obj) - Required : Object that reads from serial port
     """
-    while True:
-        # Get subject number for data directory suffix
-        try:
-            subject_num = int(input("What is the subject number?\n" ))
-            assert (subject_num >= 0), "Subject number must be a nonnegative integer."
-        except ValueError:
-            print("Subject number is not integer. Please try again...")
-
-        # Get motion name string for data directory suffix
-        try:
-            motion_num = input(
-                "Which hand motion would like to collect data for?\
-                \n 0: open-palm, 1: closed-fist\n"
-            )
-            motion_num = int(motion_num)
-            assert(motion_num in HAND_MOTIONS), "Invalid motion selection."
-        except ValueError:
-            print("Motion number is not an integer. Please try again...")
-
-        # Get trial number for csv data file suffix
-        try:
-            trial_num = int(input("What trial number is this?\n"))
-            assert(trial_num >= 0), "Trial number must be a nonnegative integer."
-            break
-        except ValueError:
-            print("Trial number is not an integer. Please try again...")
+    # Gather parameters for directory and csv creation
+    gather_parameters = prompt_dispatcher('get gather_parameters function')
+    subject_num = gather_parameters('What is the subject number?')
+    motion_num = gather_parameters('What is the motion?')
+    trial_num = gather_parameters('What is the trial number?')
 
     # Creates directories if they don't exist, but return path regardless
     data_path = create_dirs(subject_num, HAND_MOTIONS[motion_num], trial_num)
-    # If trial data already exists, ask if you wish to proceed.
-    if path_exists(data_path):
-        ans = input("{} already exists. Continue? [Y/n]\n".format(data_path))
-        if ans != 'y':
-            terminate_process(arduino)
-            return
 
-    input(
-            "Please prepare hand for data collection of {} motion.\
-            \n Press any key when you're ready to begin.\n"
-            .format(HAND_MOTIONS[motion_num])
-    )
-    collect_data(data_path, HAND_MOTIONS[motion_num], arduino)
+    # If trial data already exists, ask if you wish to proceed.
+    if not prompt_dispatcher("Is this a valid path?")(data_path):
+        return
+
+    # Prime for data collection
+    prompt_dispatcher("Prepare for data collection.")()
+    collect_data(data_path, HAND_MOTIONS[motion_num])
 
 def main():
-    arduino = connect_arduino()
-    while True:
-        begin(arduino)
-        ans = input("New trial? [y/N]\n")
-        if ans != 'y':
-            terminate_process(arduino)
-            break
+    while prompt_dispatcher("Begin a new trial?")():
+        initialize_pipeline()
 
-main()
+if __name__ == "__main__":
+    main()
